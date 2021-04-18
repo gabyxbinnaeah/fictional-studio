6f0b92d6b3505fe5ab76fc2c042ab1e83de227ae
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-04-19 01:19:21 +0300

    Adds background image using jquery

--- js/java.js
+++ js/java.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
+    // sets background image
+    $("input").val();
+    var imageUrl= "images/backgrounds/home1.jpg";
+    $("div.landing").css("background-image", "url("+imageUrl+")");
+
+    $("input").val();
+    var serviceUrl ="images/backgrounds/comp.jpg";
+    $("div.services").css("background-image", "url("+serviceUrl+")");
+
+    $("input").val();
+    var portUrl = "images/portfolio/work8.jpg";
+    $("div.portpic").css("background-image", "url("+portUrl+")");
+   
+    $("input").val();
+    var contactUrl = "images/backgrounds/c_image.jpg";
+    $("div.contact").css("background-image", "url("+contactUrl+")");
+
+
+    
 
 
 
