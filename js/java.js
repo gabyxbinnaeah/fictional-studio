7d19f44fe1725d5574f1862cf718dabdfd1681a1
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-04-19 01:23:48 +0300

    creates hover effects on portfolio pics when hovered

--- js/java.js
+++ js/java.js
@@ -17,8 +17,54 @@ $(document).ready(function() {
     $("div.contact").css("background-image", "url("+contactUrl+")");
 
 
+      //toggles what we do , display description if prompted
+
+    $("#development").click(function(){
+        $("div.development").toggle(2000);
+    });
+
+    $("#design").click(function(){
+        $("div.design").toggle(2000);
+    });
+
+    $("#product").click(function(){
+        $("div.product").toggle(2000);
+    });
+
+     //hover over portfolio
+
+     $(".work4").hover (function() {
     
-
-
+        $("div.bg-modal").fadeToggle();
+        
+     });
+ 
+    $(".work3").hover(function() {
+        $("div.bg-work3").fadeToggle();
+    });
+ 
+    $(".work2").hover(function() {
+     $("div.bg-work2").fadeToggle();
+    });
+ 
+    $(".work1").hover(function() {
+     $("div.bg-work1").fadeToggle();
+    });
+ 
+    $(".work5").hover(function() {
+     $("div.bg-work5").fadeToggle();
+    });
+ 
+    $(".work6").hover(function() {
+     $("div.bg-work6").fadeToggle();
+    });
+ 
+    $(".work7").hover(function() {
+     $("div.bg-work7").fadeToggle();
+    });
+ 
+    $(".outer8").hover(function() {
+     $("div.bg-work8").fadeToggle();
+    });
 
 });
